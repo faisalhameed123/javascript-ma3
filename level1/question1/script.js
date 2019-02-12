9b6c5b8a27ba880c6914dd7f0cd1fe78de720fe1
faisalhameed123 <44834125+faisalhameed123@users.noreply.github.com>
2019-02-12 23:04:39 +0100

    Update script.js

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,21 @@
 //Use RegEx to validate form
+<script type="text/javascript">
+	
+	var fname = document.getElementById("mohammed faisal");
+	var lname = document.getElementById("hameed");
+	var phone = document.getElementById("92698012");
+	var email = document.getElementById("mohammedhameed@hotmal.com");
+	function submit(){
+		var fnameInput = /^[a-zA-Z]+$/;
+		var fnameMatch = fname.value.match("mohammed faisal");
+		var lnameinput = /^[a-zA-Z]+$/;
+		var lnameMatch = lname.value.match("hameed");
+			if ((fnameMatch.value.length === 0 ) | (lnameMatch.value.length === 0)); {
+			alert("mohammed faisal hameed");
+			}
+		var phoneInput = /^[0-9]+$/;
+		var phoneMatch = phone.value.match(92698012);
+		var emailInput = /^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
+		var emailMatch = email.value.match("mohammedhameed@hotmail.com");
+	}
+    </script>
